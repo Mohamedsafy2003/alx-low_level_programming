6417895c8b2a9c275121a746695112be1e0f6851
Mohamed safy <mohamed.safy605@gmail.com>
2023-03-21 16:16:28 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,21 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		 _putchar('+');
+		 _putchar(',');
+		 _putchar(' ');
 		return (1);
 	} else if (n == 0)
 }
-		_putchar(48);
+		_putchar(0);
+		 _putchar(',');
+    		_putchar(' ');
 		return (0);
 	} else if (n < 0)
 	{
 		_putchar('-');
+		   _putchar(',');
+                 _putchar(' ');
 	}
 		return (-1);
 }
