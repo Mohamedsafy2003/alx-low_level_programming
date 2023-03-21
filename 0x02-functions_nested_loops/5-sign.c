c991c2692a13193cfbc8ed63ce99a242f3ce3850
Mohamed safy <mohamed.safy605@gmail.com>
2023-03-21 16:56:55 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_sign - prints the sign of a number
@@ -10,19 +9,17 @@
  */
 int print_sign(int n)
 {
-        if (n > 0)
-        {
-                _putchar('+');
-                return (1);
-        }
-       	else if (n == 0)
-        {
-                _putchar(0);
-                return (0);
-        }
-            else 
-        {
-                _putchar('-');
-                return (-1);
-        }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+}
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
 }
